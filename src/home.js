ac7ecbf63a146cacdb2d3ec2aa79e664c1c7f23c
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2024-08-07 14:49:45 +0200

    added functionality to the province dropdown menue

--- src/home.js
+++ src/home.js
@@ -1,22 +1,36 @@
 document
-  .getElementById("applicationForm")
-  .addEventListener("submit", function (event) {
+  .getElementById('applicationForm')
+  .addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const email = document.getElementById("email").value;
+    const email = document.getElementById('email').value;
 
-    emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this).then(
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this).then(
       function (response) {
-        document.getElementById("successMessage").classList.remove("hidden");
+        document.getElementById('successMessage').classList.remove('hidden');
       },
       function (error) {
-        console.error("Failed to send email", error);
+        console.error('Failed to send email', error);
       }
     );
   });
 function applyButton() {
-  document.getElementById("applyButton");
-  applyButton.addEventListener("click", function () {
-    window.location.href = "apply.html";
+  document.getElementById('applyButton');
+  applyButton.addEventListener('click', function () {
+    window.location.href = 'apply.html';
   });
 }
+
+document.getElementById('province').addEventListener('change', (event) => {
+  const selectedValue = event.target.value;
+
+  switch (selectedValue) {
+    case 'Gauteng':
+      alert(`You're attend the event in-person`);
+      break;
+    default:
+      alert(
+        `You're only eligible to attend the event virtually, because you're outside of Gauteng`
+      );
+  }
+});
